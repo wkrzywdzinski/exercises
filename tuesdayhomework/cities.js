2d727d20dbc2d0671e32ca3c38db87de8dc1700c
wkrzywdzinski <krzywdzinski.wojciech@gmail.com>
2018-10-02 19:37:43 +0200

    homework 02.10

--- tuesdayhomework/cities.js
+++ tuesdayhomework/cities.js
@@ -7,13 +7,3 @@ var a = {
 for (var b in a) {
     console.log(b + ": " + a[b]);
 }
-
-var obj = {
-    a: 1,
-    b: 2,
-    c: 3
-};
-
-for (var p in obj) {
-    console.log("The value of obj." + p + " is " + obj[p]);
-}
