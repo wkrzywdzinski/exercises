affdd1fabdc9350c6c8d7bc0f5c61e665590dbcb
wkrzywdzinski <krzywdzinski.wojciech@gmail.com>
2018-10-16 10:29:32 +0200

    out of box border problem fixed

--- week3day1homework/resizablepanes/script.js
+++ week3day1homework/resizablepanes/script.js
@@ -3,6 +3,9 @@ var leftbox = $("#leftbox");
 var outbox = $("#outbox");
 outbox.on("mousedown", function(e) {
   outbox.on("mousemove", function fun1(e) {
+    if (e.pageX >= 995) {
+      return;
+    }
     border.css("left", e.pageX + "px");
     leftbox.css("width", e.pageX + "px");
     outbox.on("mouseup", function() {
