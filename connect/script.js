4c1e3267baa546f9766da0a27c8fbdad07f1dd13
wkrzywdzinski <krzywdzinski.wojciech@gmail.com>
2018-10-21 15:15:10 +0200

    improved connect

--- connect/script.js
+++ connect/script.js
@@ -30,23 +30,18 @@ var p2wins = 0;
     [40, 33, 26, 19, 12],
     [39, 32, 25, 18]
   ];
+  //////////////////////////// VARS
   var p1name = "player1";
   var p2name = "player2";
   var p1type = $("#p1type");
-  p1type.on("input", function(e) {
-    p1name = p1type.val();
-  });
-  var p2type = $("#p2type");
-  p2type.on("input", function(e) {
-    p2name = p2type.val();
-  });
   var currentPlayer = "player1";
   var win = true;
+  var music = true;
   $(".column").on("click", function(e) {
     var col = $(e.currentTarget);
     var box = $("#box");
     var slotsInColumn = col.find(".slot");
-
+    ///////////////////////////////////// ADD CLASS
     var i;
     for (i = 5; i >= 0; i--) {
       if (
@@ -56,8 +51,8 @@ var p2wins = 0;
         break;
       }
     }
-    /////////////////////////////////////////////////////
     slotsInColumn.eq(i).addClass(currentPlayer);
+    ///////////////////////////////////////////////////// SETUP
     var slotsInRow = $(".row" + i);
     checkforwin(slotsInRow);
     checkforwin(slotsInColumn);
@@ -66,17 +61,61 @@ var p2wins = 0;
       switchPlayers();
     }
   });
-  ///////////////////////////
+  /////////////////////////// BUTTONS
   $("#againbutton").on("click", function(e) {
-    $(".playerbox").html("LET'S PLAY AGAIN. WINNER STARTS ");
+    $(".playerbox").html("LET'S PLAY AGAIN. LOSER STARTS ");
+    $("#p1wins").html(p1name + " wins: " + "<h1>" + p1wins + "</h2>");
+    $("#p2wins").html(p2name + " wins: " + "<h1>" + p2wins + "</h2>");
+    $("h1").css("opacity", 0);
+    $("h1").animate({ opacity: "1" }, "fast");
     $(".slot").removeClass("player1");
     $(".slot").removeClass("player2");
     win = true;
+    switchPlayers();
     $("#playagain").hide();
   });
   $("#playbutton").on("click", function(e) {
     $("#start").hide();
   });
+  p1type.on("input", function(e) {
+    p1name = p1type.val();
+  });
+  var p2type = $("#p2type");
+  p2type.on("input", function(e) {
+    p2name = p2type.val();
+  });
+  $("#stopmusic").on("click", function(e) {
+    if (music) {
+      $("#stopmusic").html("START MUSIC");
+      $("#music").html("");
+      music = false;
+    } else {
+      $("#stopmusic").html("STOP MUSIC");
+      music = true;
+      $("#music").html(
+        "<embed id src='music.mp3' autostart='true' loop='true' width='2' height='0'></embed>"
+      );
+    }
+  });
+  $("#resetgame").on("click", function(e) {
+    if (currentPlayer == "player1") {
+      currentPlayer == "player2";
+    }
+    if (currentPlayer == "player2") {
+      currentPlayer == "player1";
+    }
+    if (currentPlayer == "player1") {
+      $(".playerbox").html("MISCLICK? " + p1name + " STARTS");
+      $(".playerbox").css("background", "Gainsboro");
+    } else if (currentPlayer == "player2") {
+      $(".playerbox").html("MISCLICK? " + p2name + " STARTS");
+      $(".playerbox").css("background", "coral");
+    }
+    $(".slot").removeClass("player1");
+    $(".slot").removeClass("player2");
+  });
+
+  //////////////////////////////////
   function checkforwin(a) {
     var result = "";
     for (var i = 0; i < 6; i++) {
@@ -90,14 +129,13 @@ var p2wins = 0;
       if (currentPlayer == "player1") {
         $(".playerbox").html("PRETTY SNEAKY. " + p1name + " WINS");
         p1wins++;
-        $("#p1wins").html(p1name + " wins: " + p1wins);
         $("#playagain").show();
         win = false;
       }
       if (currentPlayer == "player2") {
         p2wins++;
         $(".playerbox").html("PRETTY SNEAKY. " + p2name + " WINS");
-        $("#p2wins").html(p2name + " wins: " + p2wins);
+        $("#p2wins").html(p2name + " wins: " + "<h1>" + p2wins + "</h1>");
         $("#playagain").show();
         win = false;
       }
@@ -118,18 +156,22 @@ var p2wins = 0;
         }
         if (resultb.indexOf("1111") > -1) {
           if (currentPlayer == "player1") {
-            p1wins++;
             $(".playerbox").html("PRETTY SNEAKY. " + p1name + " WINS");
+            p1wins++;
             $("#p1wins").html(p1name + " wins: " + p1wins);
             $("#playagain").show();
             win = false;
+
+            return;
           }
           if (currentPlayer == "player2") {
-            p2wins++;
             $(".playerbox").html("PRETTY SNEAKY. " + p2name + " WINS");
+            p2wins++;
             $("#p2wins").html(p2name + " wins: " + p2wins);
             $("#playagain").show();
             win = false;
+            p2wins++;
+            return;
           }
         }
       }
@@ -138,7 +180,7 @@ var p2wins = 0;
   function switchPlayers() {
     if (currentPlayer == "player1") {
       currentPlayer = "player2";
-      $(".playerbox").css("background", "Chocolate");
+      $(".playerbox").css("background", "coral");
       $(".playerbox").html(p2name + "'S TURN");
     } else {
       currentPlayer = "player1";
