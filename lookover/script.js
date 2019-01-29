99a652dff3761b75519f5a2b466226bded787981
wkrzywdzinski <krzywdzinski.wojciech@gmail.com>
2019-01-29 15:35:12 +0100

    step closer with 360 view

--- lookover/script.js
+++ lookover/script.js
@@ -1,21 +1,29 @@
 const view = $("#view");
 const view2 = $("#view2");
-const viewposition = view.position();
-const view2position = view2.position();
+var viewposition = view.position();
+var view2position = view2.position();
 var viewleft = viewposition.left;
 var view2left = view2position.left;
 $(document).on("keydown", function(e) {
-  if (view2left == 0) {
-  }
   if (e.which == 65 || e.which == 37) {
     viewleft = viewleft - 5;
     view2left = view2left - 5;
-    view.css("left", viewleft + "px");
+    if (view2left > -3) {
+      view.css("left", "-100%");
+      viewposition = view.position();
+    } else {
+      view.css("left", viewleft + "px");
+    }
     view2.css("left", view2left + "px");
   } else if (e.which == 68 || e.which == 39) {
     viewleft = viewleft + 5;
     view2left = view2left + 5;
-    view.css("left", viewleft + "px");
+    if (view2left > -3) {
+      view.css("left", "-100%");
+      viewposition = view.position();
+    } else {
+      view.css("left", viewleft + "px");
+    }
     view2.css("left", view2left + "px");
   }
 });
