bf3328d8e95bcaed0362add8eaf91144d380a752
wkrzywdzinski <krzywdzinski.wojciech@gmail.com>
2019-02-05 15:57:01 +0100

    basic vue recap

---
+++ vuetest/test/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <Todos />
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+export default {
+  name: "app",
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "todo one",
+          completed: true
+        },
+        {
+          id: 2,
+          title: "todo two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "todo three",
+          completed: true
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ vuetest/test/src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>YOYOYO</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
