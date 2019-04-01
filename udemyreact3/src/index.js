e72aa1ec0dec5a925b9ea26fce03dfae44648a7f
wkrzywdzinski <krzywdzinski.wojciech@gmail.com>
2019-04-01 21:19:33 +0200

    as before + conditional rendering

--- udemyreact3/src/index.js
+++ udemyreact3/src/index.js
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div> yoyo</div>;
-};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+        console.log(err.message);
+      }
+    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> ERROR: {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div> LATITUDE: {this.state.lat}</div>;
+    } else {
+      return <div>LOADING</div>;
+    }
+  }
+}
+
+export default App;
 ReactDOM.render(<App />, document.querySelector("#root"));
