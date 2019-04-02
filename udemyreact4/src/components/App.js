8567616397e9cf82cea3383bba172f67ec4aa722
wkrzywdzinski <krzywdzinski.wojciech@gmail.com>
2019-04-02 22:33:21 +0200

    api images search, all works

--- udemyreact4/src/components/App.js
+++ udemyreact4/src/components/App.js
@@ -1,11 +1,32 @@
 import React from "react";
+import axios from "axios";
 import Searchbar from "./Searchbar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
+  onSearchSubmit(term) {
+    // alternatively const response = await axios...
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization: `Client-ID 59842ae6823fed0b691cc0a28bb577eba306fc8cea6c64e1b2b02cf1b8497f01`
+        }
+      })
+      .then(resp => {
+        this.setState({ images: resp.data.results });
+      });
+  }
   render() {
     return (
       <div className="ui container">
-        <Searchbar />
+        <Searchbar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
--- udemyreact4/src/components/Searchbar.js
+++ udemyreact4/src/components/Searchbar.js
@@ -5,16 +5,19 @@ class Searchbar extends Component {
     super(props);
     this.state = { term: "" };
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+  onFormSubmit(e) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
   }
   onInputChange(e) {
-    console.log(e.target.value);
     this.setState({ term: e.target.value });
-    console.log(this.state.term);
   }
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="ui field">
             <label>IMAGE SEARCH</label>
             <input
